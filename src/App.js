d9980e1b655038f11750e3cd17cdfce36286ccf6
teooko <teobogdann@yahoo.com>
2024-03-31 23:13:13 +0300

    Add function for calculating fittest individuals

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import Individual from "./components/Individual";
 import Generation from "./components/Generation";
+import {useState} from "react";
 
 function App() {
     const geneArrays = [
+        [[Math.random(), Math.random(), Math.random()],
         [Math.random(), Math.random(), Math.random()],
         [Math.random(), Math.random(), Math.random()],
         [Math.random(), Math.random(), Math.random()],
         [Math.random(), Math.random(), Math.random()],
-        [Math.random(), Math.random(), Math.random()],
-        [Math.random(), Math.random(), Math.random()],
+        [Math.random(), Math.random(), Math.random()]]
     ]
+    
+    const [allGenerations, setAllGenerations] = useState(geneArrays);
     
   return (
     <div>
         Fittest individual: <Individual redness={1} roundness={1} size={1} />
-      <Generation geneArrays={geneArrays} />
-        
+        <div>
+            {allGenerations.map(generation => <Generation geneArrays={generation} setAllGenerations={setAllGenerations}/>)}
+        </div>
     </div>
   );
 }
--- src/components/Generation.jsx
+++ src/components/Generation.jsx
@@ -1,9 +1,40 @@
 ﻿import Individual from "./Individual";
+const Generation = ({geneArrays, setAllGenerations}) => {
+    
+    let fittestIndex;
 
-const Generation = ({geneArrays}) => {
+    function calculateFitness(individual) {
+        return individual.reduce((acc, trait) => {
+            return acc + trait;
+        }, 0);
+    }
+    
+    const handleNextGeneration = () => {
+        const eachFitness = geneArrays.map(
+            geneArray => calculateFitness(geneArray)
+        )
+        
+        const averageFitness = eachFitness.reduce((acc, fitness) => acc += fitness, 0) / eachFitness.length;
+        
+        
+        fittestIndex = eachFitness.map((fitness, index) => {
+            if(fitness > averageFitness)
+                return index;
+        }).filter(index => index !== undefined);
+        /*
+        for(let i = 0; i < 6; i++)
+        {
+            
+        }
+        */
+        console.log(fittestIndex);
+    }
     
     return (
         <div style={{display: "flex", flexDirection: "column", gap: 20, alignItems: "center", width: 200}}>
+            <div onClick={handleNextGeneration}>
+                Next Generation
+            </div>
             {geneArrays.map((geneArray) => <Individual redness={geneArray[0]} size={geneArray[1]} roundness={geneArray[2]} /> )}
         </div>
     )
